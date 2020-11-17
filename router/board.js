7d364aa27f3d7171e4334178fed90a6fc85a8427
seoseonyu <saddummy_@kakao.com>
2020-11-17 13:18:27 +0900

    수정

--- router/board.js
+++ router/board.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const mysql_odbc = require('../db/db_conn')();
 const conn = mysql_odbc.init();
 
-router.get('/',function(req,res){
-    var page = req.query.list;
-    var sql = "select idx, name, title, hit, date_format(modidate,'%Y-%m-%d %H:%i:%s') modidate, "+"date_format(regdate,'%Y-%m-%d %H:%i:%s') regdate from board";
+router.get('/:page',function(req,res){
+    var page = req.params.page;
+    var sql = "select idx, name, title, hit, date_format(modidate,'%Y-%m-%d %H:%i:%" +
+        "s') modidate, "+"date_format(regdate,'%Y-%m-%d %H:%i:%s') regdate from board";
     conn.query(sql, function (err,rows){
         if(err) console.error("err : " + err)
         res.render('board/board.ejs',{rows:rows,title:"부산외대 위키 | 게시판 page"+page})
@@ -13,7 +14,7 @@ router.get('/',function(req,res){
 });
 
 router.get('/',function (req,res){
-    res.redirect('/board?list=1')
+    res.redirect('/board/1')
 });
 
 router.post('/',function (req,res){
@@ -27,7 +28,7 @@ router.post('/',function (req,res){
     var sql = "insert into board(name,title,content,regdate,modidate,passwd,hit) values(?,?,?,now(),now(),?,0)";
     conn.query(sql,datas,function(err,rows){
        if(err)console.error("err : " +err);
-       res.redirect('/board?list=1');
+       res.redirect('/board/1');
     });
 });
 
--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 const  express = require('express');
 const web = express();
-
+const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -9,7 +9,7 @@ const indexRouter = require('./router/index');
 const wikiRouter = require('./router/wiki');
 const boardRouter = require('./router/board');
 
-web.set('views',__dirname + '/views');
+web.set('views',path.join(__dirname + '/views'));
 web.set('view  engine', 'ejs');
 web.engine('html', require('ejs').renderFile);
 
@@ -17,11 +17,11 @@ web.listen(8001,function (){
     console.log("Express server has started on port 8001")
 });
 
+web.use(logger('dev'));
 web.use(bodyParser.json());
 web.use(bodyParser.urlencoded({extended:false}));
 web.use(cookieParser());
-web.use(logger('dev'));
-web.use(express.static(__dirname + '/public'));
+web.use(express.static(path.join(__dirname + '/public')));
 
 web.use('/',indexRouter);
 web.use('/wiki',wikiRouter);
